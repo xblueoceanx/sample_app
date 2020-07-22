1200a4915ebb9393ab9efa87f9f4b0295c94ff99
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-07-22 15:56:11 +0900

    [Start]3-7

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,29 @@
+class TodolistsController < ApplicationController
+  def new
+  	@list = List.new
+  end
+
+  def create
+  	#1 データを新規登録するためのインスタンス作成
+  	list = List.new(list_params)
+  	#2 データをデータベースに保存するためのsaveメソッド実行
+  	list.save
+  	#3 トップ画面へリダイレクト
+  	redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+  #　ストロングパラメーター
+  def list_params
+  	params.require(:list).permit(:title, :body)
+  end
+
+end
