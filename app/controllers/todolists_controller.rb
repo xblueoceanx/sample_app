a500c590978132d4e68042f8cfc88f8a496827cf
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-07-23 14:57:41 +0900

    [Finish]3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -20,10 +20,26 @@ class TodolistsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
   private
   #　ストロングパラメーター
   def list_params
-  	params.require(:list).permit(:title, :body)
+  	params.require(:list).permit(:title, :body, :image)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '031c910a5667eccfbaaf41ff3d4341a0938ec27c5120dc30712582ee6ffc5020fea122791fc57b05b7a3ae5671084f2d3abcee3399c23dc35ca087c84f44a100'
